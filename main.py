254a11be95a13de23106f97f8c8f7193b7891aa8
한결같음 <83993373+ocxh@users.noreply.github.com>
2021-09-04 00:51:10 +0900

    add Dummy Virus and not_virus.VirusDB

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 import os
 import hashlib
+import sys
 
-eicar = 'C:/Users/gksru/Documents/programming/anti-virus/eicar.txt' 
-eicar_md5 = '44d88612fea8a8f36de82e1278abb02f' 
+eicar = 'C:/Users/gksru/Documents/programming/anti-virus/eicar.txt'
+dummy = 'C:/Users/gksru/Documents/programming/anti-virus/Dummy.txt'
+VirusDB = [
+    '67:7e8043cdf89ae1e2d54e5793f8a73a4f:EICAR Test' ,
+    '65:77bff0b143e4840ae73d4582a8914a43:Dummy Test'
+]
+vdb=[]
+vsize=[]
 
-fp = open(eicar)
-fbuf = fp.read()
-
-m = hashlib.md5()
-m.update(fbuf)
-fmd5 = m.hexdigest()
-print(fmd5)
-
-if fmd5 == eicar_md5:
-    print 'virus!!'
+def MakeVirusDB():
+    for pattern in VirusDB:
+        t=[]
+        v = pattern.split(':')
+        t.append(v[1])
+        t.append(v[2])
+        vdb.append(t)
+        
+        size = int(v[0])
+        if vsize.count(size) == 0:
+            vsize.append(size)
+        
+def SearchVDB(fmd5):
+    for t in vdb:
+        if t[0] == fmd5:
+            return True, t[1]
+    return False,''
+        
+if __name__=='__main__':
+    MakeVirusDB()
     
-else:
-    print 'no virus'
-fp.close()
+    if len(sys.argv) != 2:
+        print 'Usage : antivirus.py [file]'
+        exit(0)
+        
+    fname = sys.argv[1]
+    
+    size = os.path.getsize(fname)
+    if vsize.count(size):
+        fp = open(fname,'rb')
+        buf = fp.read()
+        fp.close()
+        
+        m = hashlib.md5()
+        m.update(buf)
+        fmd5 = m.hexdigest()
+        
+        ret, vname = SearchVDB(fmd5)
+        if ret == True:
+            print '%s %s' %(fname, vname)
+        else:
+            print '%s : ok' %(fname)
+    else:
+        print '%s : ok' %(fname)
