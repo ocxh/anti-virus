c4f29a26b7e376f2ca8aeae2db57d5b5c0919d75
한결같음 <83993373+ocxh@users.noreply.github.com>
2021-09-16 12:39:18 +0900

    create all plugin's instance

--- engine/engine_test.py
+++ engine/engine_test.py
@@ -1,4 +1,8 @@
 import kavcore.k2engine
 
 k2 = kavcore.k2engine.Engine(debug=True)
-k2.set_plugins('plugins')
+if k2.set_plugins('plugins'):
+    kav = k2.create_instance()
+    if kav:
+        print '[*] Success : create_instance'
+        
--- engine/kavcore/k2engine.py
+++ engine/kavcore/k2engine.py
@@ -1,5 +1,6 @@
 import os
 import StringIO
+import datetime
 
 import k2kmdfile
 import k2rsa
@@ -9,6 +10,9 @@ class Engine:
         self.debug = debug
         self.plugins_path = None
         self.kmdfiles = []
+        self.kmd_modules = []
+        
+        self.max_datetime = datetime.datetime(1980, 1, 1, 0, 0, 0, 0)
         
     def set_plugins(self, plugins_path):
         self.plugins_path = plugins_path
@@ -18,7 +22,6 @@ class Engine:
             return False
         
         ret = self.__get_kmd_list(plugins_path + os.sep + 'kicom.kmd', pu)
-        print ret
         if not ret:
             return False
         
@@ -26,6 +29,18 @@ class Engine:
             print '[*] kicom.kmd :'
             print '   ', self.kmdfiles
         
+        for kmd_name in self.kmdfiles:
+            kmd_path = plugins_path + os.sep + kmd_name
+            k = k2kmdfile.KMD(kmd_path, pu)
+            module = k2kmdfile.load(kmd_name.split('.')[0], k.body)
+            if module:
+                self.kmd_modules.append(module)
+                self.__get_last_kmd_build_time(k) 
+        if self.debug:
+            print '[*] kmd_modules :'
+            print '   ', self.kmd_modules
+            print '[*] Last updated %s UTC' %self.max_datetime.ctime()
+            
         return True
         
     def __get_kmd_list(self, kicom_kmd_file, pu):
@@ -48,6 +63,45 @@ class Engine:
         
         if len(kmdfiles):
             self.kmdfiles = kmdfiles
+            return True
+        else:
+            return False
+           
+    def  __get_last_kmd_build_time(self, kmd_info):
+        d_y, d_m, d_d = kmd_info.date
+        t_h, t_m, t_s = kmd_info.time
+        t_datetime = datetime.datetime(d_y, d_m, d_d, t_h, t_m, t_s)
+        
+        if self.max_datetime < t_datetime:
+            self.max_datetime = t_datetime
+    
+    def create_instance(self):
+        ei = EngineInstance(self.plugins_path, self.max_datetime, self.debug)
+        if ei.create(self.kmd_modules):
+            return ei
+        else:
+            return None
+    
+class EngineInstance:
+    def __init__(self, plugins_path, max_datetime, debug=False):
+        self.debug = debug
+        self.plugins_path = plugins_path
+        self.max_datetime = max_datetime
+        
+        self.kavmain_inst = []
+    
+    def create(self, kmd_modules):
+        for mod in kmd_modules:
+            try:
+                t = mod.KavMain()
+                self.kavmain_inst.append(t)
+            except AttributeError:
+                continue
+        
+        if len(self.kavmain_inst):
+            if self.debug:
+                print '[*] Count of KavMain : %d' %(len(self.kavmain_inst))
+            
             return True
         else:
             return False
